cb8fd3d259bdab50213733a8355b49f5a61cca49
Emir Büçkün <emirbuckun@hotmail.com>
2023-07-21 16:42:58 +0300

    Generic list created

--- GenericTypes/Program.cs
+++ GenericTypes/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Declare the generic class
+
+namespace GenericTypes
+{
+  public class GenericList<T>
+  {
+    public void Add(T input) { }
+  }
+  public class ExampleClass { }
+
+  internal class Program
+  {
+    static void Main()
+    {
+      // Declare a list of type int.
+      GenericList<int> list1 = new();
+      list1.Add(1);
+
+      // Declare a list of type string.
+      GenericList<string> list2 = new();
+      list2.Add("");
+
+      // Declare a list of type ExampleClass.
+      GenericList<ExampleClass> list3 = new();
+      list3.Add(new ExampleClass());
+    }
+  }
+}
